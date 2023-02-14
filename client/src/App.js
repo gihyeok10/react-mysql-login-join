4cea736b4261907418b6699e5b6ee106c3b642d2
kimgihyeok <gh9812301@naver.com>
2023-02-14 13:30:03 +0900

    crud

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,40 @@ import Axios from "axios";
 function App() {
   const [movieName, setMovieName] = useState("");
   const [review, setReview] = useState("");
-  const [movieReviewList,setMovieReviewList] = useState([])
-  useEffect(()=>{
-    Axios.get('http://localhost:3001/api/get').then((response)=> {
-      setMovieReviewList(response.data)
-    })
-  },[])
+  const [movieReviewList, setMovieReviewList] = useState([]);
+
+  const [newReview, setNewReview] = useState("");
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get").then((response) => {
+      setMovieReviewList(response.data);
+    });
+  }, []);
   const submitReview = () => {
     Axios.post("http://localhost:3001/api/insert", {
       movieName: movieName,
       movieReview: review,
-    }).then(() => {
-      alert("successful api");
     });
+
+    setMovieReviewList([
+      ...movieReviewList,
+      { movieName: movieName, movieReview: review },
+    ]);
   };
+
+  //두개를 분리하면서 submit시 바로 화면에 랜더링 된다.
+
+  const deleteReview = (movie) => {
+    Axios.delete(`http://localhost:3001/api/delete/${movie}`);
+  };
+
+  const updateReview = (movie) => {
+    Axios.put("http://localhost:3001/api/update", {
+      movieName: movie,
+      movieReview: newReview,
+    });
+    setNewReview("")
+  };
+
   return (
     <div className="App">
       <h1>CRUD APPLICATION</h1>
@@ -43,8 +63,26 @@ function App() {
 
         <button onClick={submitReview}>보내기</button>
 
-        {movieReviewList.map((item)=>{
-          return <h1 key={item.id}>MovieName: {item.movieName} | MovieReview: {item.movieReview}</h1>
+        {movieReviewList.map((item) => {
+          return (
+            <div className="card" key={item.id}>
+              <h1>MovieName: {item.movieName}</h1>
+              <p>MovieReview: {item.movieReview}</p>
+
+              <button
+                onClick={() => {
+                  deleteReview(item.movieName);
+                }}
+              >
+                Delete
+              </button>
+              <input type="text" id="updateInput" onChange={(e)=> {
+                setNewReview(e.target.value
+                )
+              }}/>
+              <button onClick={()=> {updateReview(item.movieName)}}>Update</button>
+            </div>
+          );
         })}
       </div>
     </div>
--- server/index.js
+++ server/index.js
@@ -17,7 +17,6 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.get('/api/get',(req,res)=>{
    const sqlSelect = "SELECT * FROM movie_reviews";
    db.query(sqlSelect, (err,result)=>{
-    console.log("이게 결과다",result)
     res.send(result)
    })
    
@@ -35,6 +34,28 @@ app.post('/api/insert',(req,res) => {
 
     })
 });
+
+app.delete('/api/delete/:movieName', (req,res)=>{
+    const name = req.params.movieName;
+    const sqlDelete = "DELETE FROM movie_reviews WHERE movieName = ?";
+    
+    db.query(sqlDelete,name, (err,result) => {
+       if(err) console.log(err);
+    })
+
+})
+
+app.put('/api/update', (req,res)=>{
+    const name = req.body.movieName;
+    const review = req.body.movieReview;
+    const sqlUpdate = "UPDATE movie_reviews SET movieName = ? WHERE movieName = ?";
+    
+    db.query(sqlUpdate,[review,name], (err,result) => {
+       if(err) console.log(err);
+    })
+
+})
+
 app.listen(3001,()=>{
     console.log("달려요 포트 3001")
 })
